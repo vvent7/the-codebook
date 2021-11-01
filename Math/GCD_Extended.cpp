9d7515fd7ade981379a65da192a68eb71847ea1d
jonatas <jonatas.games@hotmail.com>
2021-11-01 16:37:18 -0400

    GCD estendido recomentado

--- Math/GCD_Extended.cpp
+++ Math/GCD_Extended.cpp
@@ -2,24 +2,20 @@
 #define _ ios_base::sync_with_stdio(0);
 using namespace std;
 
+//funciona para m não primo
 int modInverse(int a, int m){
     int m0 = m;
     int y = 0, x = 1;
     if (m == 1)
       return 0;
     while (a > 1){
-        // q is quotient
         int q = a / m;
         int t = m;
-        // m is remainder now, process same as
-        // Euclid's algo
         m = a % m, a = t;
         t = y;
-        // Update y and x
         y = x - q * y;
         x = t;
     }
-    // Make x positive
     if (x < 0)
        x += m0;
     return x;
