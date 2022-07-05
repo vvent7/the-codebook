8624282dd3f09a6e2f765cd0d22cab95fc91e019
theneverchosen <fernandoventilari@gmail.com>
2022-07-05 01:58:39 -0400

    kadane

--- paradigms/kadane.cpp
+++ paradigms/kadane.cpp
@@ -3,7 +3,7 @@ using namespace std;
 typedef long long ll;
 
 //maior soma em subsequências contíguas
-ll kadane(int v[], size_t n){
+ll kadane(int *v, size_t n){
 	ll best=0, acum=0;
 	for(size_t i=0;i<n;i++){
 		acum=max(ll(v[i]), acum+v[i]);
