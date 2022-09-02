af54f0d5f35412dc735865cb48b8a15505092408
Fernando Ventilari <fernandoventilari@gmail.com>
2022-09-02 04:38:38 -0400

    prefix_sum added, .cpp added to bit file

---
+++ structures/prefix_sum.cpp
@@ -0,0 +1,19 @@
+typedef long long ll;
+#define MAX_N 200005
+
+ll pre[MAX_N];
+
+void unbuild(int n){ //unbuild in-place
+  for(int i=n-1;i>0;i--)
+    pre[i]-=pre[i-1];
+}
+
+void build(int n){ //build in-place
+  for(int i=1;i<n;i++)
+    pre[i]+=pre[i-1];
+}
+
+ll sum_range(int l, int r){
+  if(l==1) return pre[r];
+  return pre[r]-pre[l-1];
+}
---
+++ structures/prefix_sum_2d.cpp
@@ -0,0 +1,34 @@
+typedef long long ll;
+#define MAX_N 1005
+#define MAX_M 1005
+
+ll pre[MAX_N][MAX_M];
+
+void unbuild(int n, int m){ //unbuild in-place
+  for(int i=n-1;i>0;i--)
+    for(int j=m-1;j>0;j--)
+      pre[i][j]-=pre[i][j-1]+pre[i-1][j]-pre[i-1][j-1];
+  
+  for(int i=n-1;i>0;i--) pre[i][0]-=pre[i-1][0];
+  for(int j=m-1;j>0;j--) pre[0][j]-=pre[0][j-1];
+}
+
+void build(int n, int m){ //build in-place
+  for(int i=1;i<n;i++) pre[i][0]+=pre[i-1][0];
+  for(int j=1;j<m;j++) pre[0][j]+=pre[0][j-1];
+
+  for(int i=1;i<n;i++)
+    for(int j=1;j<m;j++)
+      pre[i][j]+=pre[i][j-1]+pre[i-1][j]-pre[i-1][j-1];
+}
+
+ll sum(int i, int j){
+  if(i<0 && j<0) return 0;
+  if(i<0) return pre[0][j];
+  if(j<0) return pre[i][0];
+  return pre[i][j];
+}
+
+ll sum_range(int i, int j, int k, int l){
+  return sum(k,l)-sum(k, j-1)-sum(i-1, l)+sum(i-1, j-1);
+}
