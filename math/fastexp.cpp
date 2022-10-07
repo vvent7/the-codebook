2b0c2dededc55abc148cf5c011649979ef9a097b
Fernando Ventilari <fernandoventilari@gmail.com>
2022-10-07 15:05:42 -0400

    fastexp fix

--- math/fastexp.cpp
+++ math/fastexp.cpp
@@ -1,8 +1,14 @@
+#define MOD 1000000009LL
 typedef long long ll;
 
-ll fast_exp(ll b, ll exp){
-  if(exp==0) return 1LL;
-  if(b&1) return b*fast_exp(b, exp-1);
-  ll x=fast_exp(b, exp/2);
-  return x*x;
+ll mod(ll a, ll m){
+  return ((a%m)+m)%m;
+}
+
+ll modPow(ll b, ll p){
+  if(p == 0) return 1;
+  ll ans = modPow(b, p>>1);
+  ans = mod(ans*ans,MOD);
+  if(p&1) ans = mod(ans*b, MOD);
+  return ans;
 }
